5349e47fa7e44b9507298722d4966c19728b7f07
wagoner47 <wagoner47@email.arizona.edu>
2017-04-24 15:09:39 -0700

    Added comments to function do_something in code.py

--- code.py
+++ code.py
@@ -2,5 +2,15 @@
 import datetime
 
 def do_something():
+    """do_something
+    
+    This code prints the current date and time.
+    
+    Arguments:
+    None
+    
+    Output:
+    Print the date and time with a message
+    """
     time = datetime.datetime.now()
     print('Hello! The date and time is', time)
